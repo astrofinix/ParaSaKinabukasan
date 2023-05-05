40fd7021e3761718b526426f6d784c4b36c65c9c
J.Pean <astrofphinix@gmail.com>
2023-05-05 12:15:12 +0800

    samplre_preview, navbar, number marker, title markers

---
+++ js/script1.js
@@ -0,0 +1,12 @@
+var menuBtn = document.getElementById('menuBtn')
+      var sideNav = document.getElementById('sideNav')
+
+      sideNav.style.right = '-40vw'
+
+      menuBtn.onclick = function () {
+        if (sideNav.style.right == '-40vw') {
+          sideNav.style.right = '0'
+        } else {
+          sideNav.style.right = '-40vw'
+        }
+      }
